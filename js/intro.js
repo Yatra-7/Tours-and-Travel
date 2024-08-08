c9d697f0a9bb35a94cd9244673791ce18c462da3
Yatra Prajapati <yatraprajapati6@gmail.com>
2024-08-08 22:21:13 +0530

    tours and travel website
    Using HTML,CSS,JS,PHP,SQL

---
+++ js/intro.js
@@ -0,0 +1,134 @@
+// console.log("welcome to js")
+// var myAge=21;
+// console.log(myAge);
+// var my_firstname="yatra";
+
+// var_myLastname="prp";
+// console.log(mylastname)
+
+// var 123myAGE =21;
+
+// var $cityname="new yrk";
+// console.log($cityname);
+
+// console.log(parseInt("123"))
+
+
+// console.log(parseInt("123,10"))
+
+
+// console.log(parseInt("077"))
+
+
+// console.log(parseInt("  123 "))
+
+
+// console.log(parseFloat("1.9"))
+// console.log(parseInt("&123"))
+// console.log(parseInt("-123"))
+// console.log(parseInt("xyz"))
+
+// console.log(isNaN("5"));
+
+// let userAge = 22;
+// let isCitizen = true;
+// let isRegistered = false;
+
+// if (userAge>= 18){
+//     if(isCitizen){
+//         if(isRegistered){
+//             console.log("you are eligible to vote");
+//         }
+//             else{
+//                 console.log("you are not eligible due to reg. status");
+//             }   
+//     }
+//     else{
+//         console.log("you are eligible due to citizenship status");
+//     }
+// }
+//     else{
+//         console.log("You are not eligible to vote");
+// }
+
+// var day ="tuesday";
+
+// switch (day){
+//     case "monday":
+//         console.log("today is monday");
+//     break;
+//     case "friday":
+//         console.log("lets party");
+//     break;
+//     default: console.log("no condition");
+//     break;
+// }
+
+// var areaofShapes="round";
+// var a=2;
+// var b=3;
+// var result;
+// switch (areaofShapes){
+//     case"square":
+//     result=a*a;
+//     console.log(result);
+//     break;
+//     case"rectangle":
+//     result=a*b;
+//     console.log(result);
+//     break;
+//     case"circle":
+//     var r= 2;
+//     result=3.142*r*r;
+//     console.log(result);
+//     break;
+//     default:console.log("no shape matches");
+//     break;
+// }
+
+// var num =1;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+// var num=1;
+// do{
+//     console.log(num);
+//     num++;
+// }while(num<=10);
+
+// var num=1;
+// for(var num=1; num<=10;num++ )
+// {
+//     console.log(num)
+// }
+
+// var num=5;
+// while(num<=10){
+//     console.log('5 * '+ num + '=' + 5*num)
+//     num++;
+// }
+
+// let userInput;
+// let positiveNumber;
+// do{
+//     userInput = prompt('enter any positive number')
+// }while()
+
+// for (var i=1; i<=5; i++){
+//     var pattern = "";
+//     for (var j=1; j<=i; j++){
+//         pattern=pattern +" * ";
+//     }
+//     console.log(pattern);
+// }
+ 
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(5,10);
+// let text ="Hello JavaScript,Welcome to our world best Javascript course";
+// let index = text.lastIndexOf("JavaScript",40);
+// console.log(index);
+
